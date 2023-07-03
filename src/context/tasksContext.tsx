4420bc0c43b8f5b6e0f84caf3733352a28fc5859
Rafael Kramer da Silva <rafael.silva@grupom8.local>
2023-07-03 17:58:11 -0300

    update

--- src/context/tasksContext.tsx
+++ src/context/tasksContext.tsx
@@ -1,36 +1,56 @@
+// Importando as funções e tipos necessários do pacote 'react' e o uuidv4 do pacote 'uuid'
 import { ReactNode, createContext, useContext, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
+// Definindo a interface para a estrutura da tarefa
 interface ITask {
   id: string;
   title: string;
   description: string;
 }
 
+// Definindo a interface para o contexto que será usado para gerenciar as tarefas
 interface ITasksContext {
-  task: ITask[];
-  addTask: (taskId: string) => void;
-  removeTask: (taskId: string) => void;
+  tasks: ITask[];
+  addTask: (title: string, description: string) => void;
+  removeTask: (id: string) => void;
 }
 
+// Criando o contexto com a interface ITasksContext
 const TasksContext = createContext<ITasksContext>({} as ITasksContext);
 
+// Definindo o tipo de propriedades que o TasksProvider aceita
 type TasksProviderProps = {
   children: ReactNode;
 };
 
+// Definindo o componente TasksProvider
 const TasksProvider = ({ children }: TasksProviderProps) => {
+  // Utilizando o hook useState para gerenciar as tarefas
   const [tasks, setTasks] = useState<ITask[]>([]);
 
+  // Definindo a função para adicionar uma tarefa
   const addTask = (title: string, description: string) => {
+    const newTask = {
+      id: uuidv4(), // Gerando um id único para a tarefa
+      title,
+      description,
+    };
+
+    // Atualizando o estado de tarefas para incluir a nova tarefa
     setTasks((prevTasks) => [
-      // Spread Operator - mantém as tasks atuais e acrescenta uma nova task no final
-      ...prevTasks
-    ])
+      ...prevTasks, // Mantendo as tarefas anteriores
+      newTask, // Adicionando a nova tarefa no final da lista
+    ]);
   };
 
-  const removeTask = (taskId: string) => { };
+  // Definindo a função para remover uma tarefa
+  const removeTask = (id: string) => {
+    // Atualizando o estado de tarefas para remover a tarefa com o id fornecido
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id != id));
+  };
 
+  // Retornando o Provedor de contexto que contém as tarefas e as funções para adicionar e remover tarefas
   return (
     <TasksContext.Provider value={{ tasks, addTask, removeTask }}>
       {children}
@@ -38,12 +58,17 @@ const TasksProvider = ({ children }: TasksProviderProps) => {
   );
 };
 
-export const useTasks(): ITasksContext {
+// Definindo o hook personalizado useTasks para acessar facilmente o contexto de tarefas
+const useTasks = () => {
   const context = useContext(TasksContext);
+
+  // Garantindo que o hook esteja sendo usado dentro de um TasksProvider
+  if (context === undefined) {
+    throw new Error("useTasks must be used within a TasksProvider");
+  }
+
   return context;
-}
+};
 
+// Exportando o TasksProvider e o hook useTasks para serem usados em outros componentes
 export { TasksProvider, useTasks };
-
-// let id = uuidv4();
-// console.log(id);
